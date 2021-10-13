8b3eadf92bb2823145f9a5014fb8e8dc91286b88
PedroLima855 <pedro.felix@publicacoesonline.com.br>
2021-10-13 08:55:56 -0300

    "criado activity de perfil do usuario"

--- app/src/main/java/com/my/appcommerce/UserProfileActivity.kt
+++ app/src/main/java/com/my/appcommerce/UserProfileActivity.kt
@@ -2,10 +2,30 @@ package com.my.appcommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class UserProfileActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+    lateinit var textTitle: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        textTitle = findViewById(R.id.toolbar_title)
+        textTitle.text = getString(R.string.user_profile_title)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true;
     }
 }
